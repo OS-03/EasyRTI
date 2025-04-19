69436392eac71be04fdb8dfc230a2862fa5cccfe
bob <ferozshaikh2222@gmail.com>
2025-04-20 05:24:47 +0530

    Final Project Commit

--- backend/routes/chat.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import isAuthenticated from "../middlewares/isAuthenticated.js";
-import chat from "../controllers/chat.controller.js"; // Ensure correct file extension
-
-const router = express.Router();
-
-router.route("/").post(chat);
-
-export default router;
